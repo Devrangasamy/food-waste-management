607a2e1f4c77635bd4351712f0dd022f9d2a45fc
Ashwinth <ashwinthk.20cse@kongu.edu>
2022-10-16 22:33:23 +0530

    register

--- Client/src/Pages/Registernew.js
+++ Client/src/Pages/Registernew.js
@@ -1,63 +1,111 @@
-import React from 'react';
-// import axios from 'axios';
-// class App extends Component{
-//     constructor(){
-//         super();
-//         this.state={
-//             fullname:'',
-//             username:'',
-//             email:'',
-//             password:''
-//         };
-//         this.changefullname = this.changefullname.bind(this);
-//         this.changeusername=this.changeusername.bind(this);
-//         this.changeemail=this.changeemail.bind(this);
-//         this.changepassword=this.changepassword.bind(this);
-//         this.onSubmit=this.onSubmit.bind(this);
-//     }
+import React, {Component} from 'react';
+import axios from 'axios';
 
-//     changefullname(event){
-//         this.setState({
-//             fullname:event.target.value
-//         });
 
-//     }
-//     changeusername(event){
-//         this.setState({
-//             username:event.target.value
-//         });
-//     }
-//     changeemail(event){
-//         this.setState({
-//             email:event.target.value
-//         });
-//     }
-//     changepassword(event){
-//         this.setState({
-//             password:event.target.value
-//         });
-//     }
+class Registernew extends Component{
+    constructor(){
+        super();
+        this.state={
+            username:'',
+            email:'',
+            password:'',
+            confirmpassword:'',
+            age:'',
+            birthdate:'',
+            phonenumber:'',
+            gender:'',
+            address:''
+        };
+        this.changeusername=this.changeusername.bind(this);
+        this.changeemail=this.changeemail.bind(this);
+        this.changepassword=this.changepassword.bind(this);
+        this.changeconfirmpassword=this.changeconfirmpassword.bind(this);
+        this.changeage=this.changeage.bind(this);
+        this.changebirthdate=this.changebirthdate.bind(this);
+        this.changephonenumber=this.changephonenumber.bind(this);
+        this.changegender=this.changegender.bind(this);
+        this.changeaddress=this.changeaddress.bind(this);
+        this.onSubmit=this.onSubmit.bind(this);
+    }
 
-//     onSubmit(event){
-//         event.preventDefault()
+ 
+    changeusername(event){
+        this.setState({
+            username:event.target.value
+        });
+    }
+    changeemail(event){
+        this.setState({
+            email:event.target.value
+        });
+    }
+    changepassword(event){
+        this.setState({
+            password:event.target.value
+        });
+    }
+    changeconfirmpassword(event){
+        this.setState({
+            confirmpassword:event.target.value
+        });
+    }
+    changeage(event){
+        this.setState({
+            age:event.target.value
+        });
+    }
+    changebirthdate(event){
+        this.setState({
+            birthdate:event.target.value
+        });
+    }
+    changephonenumber(event){
+        this.setState({
+            phonenumber:event.target.value
+        });
+    }
+    changegender(event){
+        this.setState({
+            gender:event.target.value
+        });
+    }
+    changeaddress(event){
+        this.setState({
+            address:event.target.value
+        });
+    }
 
-//         const registered ={
-//             fullname:this.state.fullname,
-//             username:this.state.username,
-//             email:this.state.email,
-//             password:this.state.password
-//         }
-//         axios.post('http://localhost:3000/api/v1/users/signup',registered)
-//             .then(response=>console.log(response.data))
-//         this.setState({
-//             fullname:'',
-//             username:'',
-//             email:'',
-//             password:''
-//         })
-//     }
+    onSubmit(event){
+        event.preventDefault()
 
-export const Registernew = () => {
+        const registered ={
+            username:this.state.username,
+            email:this.state.email,
+            password:this.state.password,
+            confirmpassword:this.state.confirmpassword,
+            age:this.state.age,
+            birthdate:this.state.birthdate,
+            phonenumber:this.state.phonenumber,
+            gender:this.state.gender,
+            address:this.state.address
+
+        }
+        axios.post('http://localhost:3000/api/users/signup',registered)
+            .then(response=>console.log(response.data))
+        this.setState({
+            username:'',
+            email:'',
+            password:'',
+            confirmpassword:'',
+            age:'',
+            birthdate:'',
+            phonenumber:'',
+            gender:'',
+            address:''
+        })
+    }
+
+render(){
   return (
     <div className='container'>
             <div className='app-wrapper'>
@@ -66,40 +114,40 @@ export const Registernew = () => {
                         Create Account
                     </h2>
                 </div>
-                <form className='form-wrapper' >
+                <form className='form-wrapper' onSubmit={this.onSubmit} >
                     <div className='name'>
                         <label className='label'>User name</label>
-                        <input className='input' type="text" />
+                        <input className='input' type="text" onChange={this.changeusername} value={this.state.username}/>
                     </div>
                     <div className='email'>
                         <label className='label'>Email</label>
-                        <input className='input' type="email" />
+                        <input className='input' type="email" onChange={this.changeemail} value={this.state.email}/>
                     </div>
                     <div className='password'>
                         <label className='label'>password</label>
-                        <input className='input' type="password" />
+                        <input className='input' type="password" onChange={this.changepassword} value={this.state.password}/>
                     </div>
                     <div className='password'>
                         <label className='label'>Confirm password</label>
-                        <input className='input' type="password" />
+                        <input className='input' type="password" onChange={this.changeconfirmpassword} value={this.state.confirmpassword}/>
                     </div>
                     <div className='num'>
                         <label className='label'>Age</label>
-                        <input className='input' type="number" />
+                        <input className='input' type="number" onChange={this.changeage} value={this.state.age}/>
                     </div>
                     <div className='num'>
                         <label className='label'>Birth date</label>
                         <input
                             type="text"
                             className='input'
-                            onChange={(e) => console.log(e.target.value)}
+                            onChange={this.changebirthdate} value={this.state.birthdate}
                             onFocus={(e) => (e.target.type = "date")}
                             onBlur={(e) => (e.target.type = "text")}
                         />
                     </div>
                     <div className='name'>
                         <label className='label'>Phone Number</label>
-                        <input className='input' type="text"/>
+                        <input className='input' type="text" onChange={this.changephonenumber} value={this.state.phonenumber}/>
                     </div>
                     <div className="gender">
                         <label className='label'>Gender:</label>
@@ -109,7 +157,7 @@ export const Registernew = () => {
                     </div>
                     <div className='name'>
                         <label className='label'>Address</label>
-                        <input className='input' type="textarea"/>
+                        <input className='input' type="textarea" onChange={this.changeaddress} value={this.state.address}/>
                     </div>
 
                     
@@ -121,4 +169,7 @@ export const Registernew = () => {
     
         </div>
   );
-};
+}
+}
+
+export default Registernew;
