17d24795fe31c1f719428b993ba05f4800bbd91b
DINESHBABU R <90496628+DINESHBABU2002@users.noreply.github.com>
2022-12-25 11:46:45 +0530

    Date added

--- Client/src/Pages/Admin/Listedfoods.js
+++ Client/src/Pages/Admin/Listedfoods.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Loading from "../Loading";
-import "./List.css"
+import "./List.css";
 export const Listedfoods = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
@@ -28,34 +28,33 @@ export const Listedfoods = () => {
   }
 
   return (
-    <div >
-      
-      <h1 style={{ color: 'black' }} className="head">Listed Foods</h1>
+    <div>
+      <h1 style={{ color: "black" }} className="head">
+        Listed Foods
+      </h1>
 
       <div className="car">
-      
-        {data.map((singleData) => (
-         
-            singleData.fooddetails.map((food) => (
-              
-              <div className="containe">
-                
-                <div className="cards">
-                  <div className="card">
-                    <h3>Name:{singleData.userid[0].name}</h3>
-                    <p>Food:{food.text}</p>
-                    <p>Quantity:{food.number}</p>
-                    <p>Contact:{singleData.mobile}</p>
-                  </div>
+        {data.map((singleData) =>
+          singleData.fooddetails.map((food) => (
+            <div className="containe">
+              <div className="cards">
+                <div className="card">
+                  <h3>Name:{singleData.userid[0].name}</h3>
+                  <p>Food:{food.text}</p>
+                  <p>Quantity:{food.number}</p>
+                  <p>Contact:{singleData.mobile}</p>
+                  <p>
+                    Date:{new Date(singleData.createdAt).toLocaleDateString()}
+                  </p>
+                  <p>
+                    Time:{new Date(singleData.createdAt).toLocaleTimeString()}
+                  </p>
                 </div>
               </div>
-             
-            ))
-
-         
-        ))}
-        </div>
-      
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
